fabfa916a939ddabd7cc92aeafda455f912eca54
albitar22 <fadi.albitar@grupposcai.it>
2023-05-19 16:50:53 +0200

    103

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomePageComponent } from './home-page/home-page.component';
 import { MenuComponent } from './menu/menu.component';
-import { CartComponent } from './cart/cart.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     MenuComponent,
-    CartComponent,
+    ProductDetailsComponent,
   ],
   imports: [
     BrowserModule,
