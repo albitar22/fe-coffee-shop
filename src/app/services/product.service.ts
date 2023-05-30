9c77c3429844085a4d8fd39b9344ca939b9a66bc
albitar22 <fadi.albitar@grupposcai.it>
2023-05-30 10:12:57 +0200

    202

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -7,10 +7,10 @@ import { Product } from '../models/product';
 })  
 export class ProductService {  
   productList: Array<Product> = ([  
-    { id: 1, name: 'Mobile', price: 12000, description: 'New Mobile', quantity: 2 },  
-    { id: 2, name: 'Computer', price: 30000, description: 'New Computer',quantity: 4 },  
-    { id: 3, name: 'Cabinet', price: 800, description: 'New Cabinet' ,quantity: 1},  
-    { id: 4, name: 'HDD', price: 1000, description: 'New HDD', quantity: 2 }  
+    { id: 1, name: 'coffee1', price: 12000, description: 'New Mobile', quantity: 2 },  
+    { id: 2, name: 'coffee2', price: 30000, description: 'New Computer',quantity: 4 },  
+    { id: 3, name: 'coffee3', price: 800, description: 'New Cabinet' ,quantity: 1},  
+    { id: 4, name: 'coffee4', price: 1000, description: 'New HDD', quantity: 2 }  
   
   ])  
   constructor() { }  
