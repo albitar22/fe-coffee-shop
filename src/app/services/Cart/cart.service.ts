115df4ae1634ec0f46095d6a8db50ac18b01b171
albitar22 <fadi.albitar@grupposcai.it>
2023-07-10 10:56:58 +0200

    123

--- src/app/services/Cart/cart.service.ts
+++ src/app/services/Cart/cart.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, lastValueFrom } from 'rxjs';
 import { Product } from '../../models/product';
 
 @Injectable({
@@ -10,12 +10,14 @@ export class CartService {
   constructor(private http: HttpClient) {}
 
   getCartItems(): Observable<Product[]> {
-    return this.http.get<Product[]>('http://localhost:8080/cart/');
+    return this.http.get<Product[]>('http://localhost:8080/api/carts/');
   }
+
 
   async addToCart(product: Product): Promise<any> {
     try {
-      const response = await this.http.post('http://localhost:8080/api/carts/', product).toPromise();
+      const observable = this.http.post('http://localhost:8080/api/carts/', product);
+      const response = await lastValueFrom(observable);
       return response;
     } catch (error) {
       if (error instanceof HttpErrorResponse) {
@@ -26,12 +28,13 @@ export class CartService {
       throw error;
     }
   }
+  
 
   removeFromCart(itemId: number): Observable<any> {
-    return this.http.delete(`http://localhost:8080/cart/${itemId}`);
+    return this.http.delete(`http://localhost:8080/api/carts/${itemId}`);
   }
 
   getCartTotal(): Observable<number> {
-    return this.http.get<number>('http://localhost:8080/cart/total');
+    return this.http.get<number>('http://localhost:8080/api/carts/total');
   }
 }
